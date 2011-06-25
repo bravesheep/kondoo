b1ed5ef9778a133bda9261868447be135cdb557e
Ruben Nijveld <ruben@gewooniets.nl>
2011-06-25 21:30:19 +0200

    Added documentation for Application.php and made some minor changes

--- lib/Kondoo/Application.php
+++ lib/Kondoo/Application.php
@@ -12,18 +12,33 @@ use \Kondoo\Response;
 
 /**
  * The base application class. All applications should extend from this class.
+ * The extended class may contain any number of setup* methods, where * is a
+ * camel cased resource name. Use the {@link beforeSetup()} method to initialize
+ * settings you need to configure before any setup method is executed. This may be mainly
+ * used for loading a configuration file. Note that before and during setup, the request
+ * and response are not yet available.
  */
 abstract class Application {
-    
+    /**
+     * Method prefix for setup functions.
+     * @var string
+     */
     const SETUP_METHOD_IDENTIFIER = "setup";
     
 	/**
 	 * Contains the latest instance of the application after the run function has dispatched
 	 * the request to a dispatcher
-	 * @var Kondoo\Application
+	 * @var \Kondoo\Application
 	 */
 	private static $application = null;
 	
+	/**
+	 * Initialize a new application. This sets all the base options using {@link setBaseOptions()},
+	 * then calls the {@link beforeSetup} method, and finally calls all setup methods. This 
+	 * function also sets the default timezone, preventing PHP error messages.
+	 * @param string $environment The environment (eg. production, development)
+	 * @return void
+	 */
 	public static function init($environment)
 	{
 	    static::setBaseOptions($environment);
@@ -42,6 +57,8 @@ abstract class Application {
 	 * This method sets some basic options such as the app location and public folder. These
 	 * options may be overwritten in a config file, but in most cases the default locations
 	 * should suffice.
+	 * @param string $environment The environment (eg. production, development)
+	 * @return void
 	 */
 	protected static function setBaseOptions($environment)
 	{
@@ -72,33 +89,69 @@ abstract class Application {
 	    return strtoupper($name[0]) . substr($name, 1);
 	}
 	
+	/**
+	 * Names of resources that are currently being invoked.
+	 * @var array
+	 */
 	private $invoked = array();
+	
+	/**
+	 * Currently loaded resources.
+	 * @var array
+	 */
 	private $resources = array();
 	
+	/**
+	 * The currently active request. Only active during a call to {@link run()}.
+	 * @var \Kondoo\Request
+	 */
 	public $request;
+	
+	/**
+	 * The current response output. Only active during a call to {@link run()}.
+	 * @var \Kondoo\Response
+	 */
 	public $response;
 	
 	/**
-	 * Run all setup methods in the Application. Note that some setup functions may require some
-	 * resource to be constructed. The construction of a method may 
+	 * Return all methods that qualify as a setup method.
+	 * @return array An array of \RelfectionMethod objects.
+	 */
+	private function getSetupMethods()
+	{
+	    $reflector = new \ReflectionClass($this);
+	    $mid = self::SETUP_METHOD_IDENTIFIER;
+	    return array_filter($reflector->getMethods(), function($method) use($mid) {
+	        return strpos($method->getName(), $mid) === 0 &&
+	            $method->getNumberOfRequiredParameters() === 0;
+	    });
+	}
+	
+	/**
+	 * Run all setup methods in the Application. If a resource setup function recursively tries to
+	 * call itself, it is prevented from doing so, after checking if a Provider resource might be
+	 * constructed using {@link initResource()}. If a setup function returns a value that
+	 * evaluates to true in PHP, it is interpreted as a resource with the same name as the setup
+	 * function. Otherwise the value is ignored.
+	 * @see hasResource()
+	 * @see initResource()
+	 * @see resource()
+	 * @see \Kondoo\Resource\Provider
+	 * @return void
 	 */
 	private function runSetupFunctions()
 	{
 	    $this->initialized = array();
 	    
-	    $reflector = new \ReflectionClass($this);
-	    $mid = self::SETUP_METHOD_IDENTIFIER;
-	    $methods = array_filter($reflector->getMethods(), function($method) use($mid) {
-	        return strpos($method->getName(), $mid) === 0 &&
-	            $method->getNumberOfRequiredParameters() === 0;
-	    });
-	    
+	    $methods = $this->getSetupMethods();
 	    foreach($methods as $method) {
 	        $resourceName = substr($method->getName(), strlen(self::SETUP_METHOD_IDENTIFIER));
+	        $resourceName = self::toResourceName($resourceName);
+	        
 	        if(!$this->hasResource($resourceName)) {
 	            $this->invoked[$method->getName()] = true;
     	        $resource = $this->{$method->getName()}();
-    	        if(isset($resource) && $resource) {
+    	        if($resource) {
     	            $this->resources[$resourceName] = $resource;
     	        }
     	        $this->invoked[$method->getName()] = false;
@@ -107,7 +160,9 @@ abstract class Application {
 	}
 	
 	/**
-	 *
+	 * Return true if a resource with the given name exists and is initialized (and ready to use).
+	 * @param string $name The name of the resource
+	 * @return boolean True if the resource exists and is initialized, false otherwise.
 	 */
 	public function hasResource($name) {
 	    $name = self::toResourceName($name);
@@ -115,35 +170,51 @@ abstract class Application {
 	}
 	
 	/**
+	 * Return a setup method for the given (resource) name. A setup method is a match if the lower
+	 * case versions of the method name and the constructed method name (from the given name) are
+	 * the same.
+	 * @param string $name The name of a setup method (without the setup prefix).
+	 * @return \ReflectionMethod The matching method, or null if none was found.
+	 */
+	private function getSetupMethod($name)
+	{
+	    $name = self::toResourceName($name);
+	    $methods = $this->getSetupMethods();
+	    $fullMethodName = strtolower(self::SETUP_METHOD_IDENTIFIER . $name);
+	    
+	    foreach($methods as $method) {
+	        if(strtolower($method->getName()) === $fullMethodName) {
+	            return $method;
+	        }
+	    }
+	    return null;
+	}
+	
+	/**
 	 * Return a resource for the given name. If the resource is not yet initialized, it is 
 	 * automatically initialized. If a setup method for the same resource as the requested resource
 	 * is currently running, the framework searches for another object providing the resource.
+	 * @param string $name The name of the resource.
+	 * @return mixed The value of the requested resource.
 	 */
 	public function resource($name)
 	{
 	    $name = self::toResourceName($name);
 	    if(!$this->hasResource($name)) {
-	        $methodName = self::SETUP_METHOD_IDENTIFIER . $name;
-	        $reflector = new \ReflectionClass($this);
-	        if($reflector->hasMethod($methodName) && !$this->invoked[$methodName]) {
-	            $methodReflector = $reflector->getMethod($methodName);
-	            
-	            $this->invoked[$methodName] = true;
-                $resource = $this->{$methodReflector->invoke()}();
-                $this->invoked[$methodName] = false;
+	        $method = $this->getSetupMethod($name);
+	        if(!is_null($method) && !$this->invoked[$method->getName()]) {	            
+	            $this->invoked[$method->getName()] = true;
+                $resource = $this->{$method->getName()}();
+                $this->invoked[$method->getName()] = false;
 
                 if($resource) {
                     $this->resources[$name] = $resource;
                 } else {
-                    throw new \Exception(sprintf(
-                        _("Resource method %s returned no resource."), 
-                        $resourceName
-                    ));
+                    $this->initResource($name);
                 }
 	        } else {
 	            $this->initResource($name);
 	        }
-	        
 	    }
 	    return $this->resources[$name];
 	}
@@ -161,9 +232,10 @@ abstract class Application {
 	    $resourceName = self::toResourceName($name);
 	    
 	    $resourceProviders = Options::get('app.resources', array());
+	    $configResourceName = strtolower($resourceName);
         $first = ArrayUtil::firstKey($resourceProviders, 
-            function($key) use ($resourceName)  {
-                return strtolower($key) === strtolower($resourceName);
+            function($key) use ($configResourceName)  {
+                return strtolower($key) === $configResourceName;
             }
         );
         
@@ -174,7 +246,7 @@ abstract class Application {
                 throw new \Exception(sprintf(
                     _("%s is not an instance of %s"),
                     $resourceClass,
-                    "Kondoo\\Resource\\Provider"
+                    "\\Kondoo\\Resource\\Provider"
                 ));
             }
             $resourceOptions = Options::get($first, array());
@@ -189,7 +261,7 @@ abstract class Application {
 	}
 	
 	/**
-	 * Shorthand for the resource method, using PHP magic methods.
+	 * Shorthand for the {@link resource()} method, using PHP magic methods.
 	 * @see resource()
 	 * @param string $name The name of a resource.
 	 * @return mixed The requested resource.
@@ -200,7 +272,7 @@ abstract class Application {
 	}
 	
 	/**
-	 * Shorthand for the hasResource method, using PHP magic methods.
+	 * Shorthand for the {@link hasResource()} method, using PHP magic methods.
 	 * @see hasResource()
 	 * @param string $name The name of a resource.
 	 * @return boolean True if the resource exists and is initialized, false otherwise.
@@ -211,7 +283,9 @@ abstract class Application {
 	}
 	
 	/**
-	 * Default implementation: does nothing.
+	 * This function is called just before all setup methods are called. Use it to set configuration
+	 * settings and similar. Default implementation: tries to load the config.yaml file.
+	 * @return void
 	 */
 	protected function beforeSetup()
 	{
